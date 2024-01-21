0989c8eb3a4656aded67d4605f6bb0d4491583eb
mano3queijos <emanuel.almeida@ucsal.edu.br>
2024-01-21 19:50:55 -0300

    jogo incompleto

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum + 1, randomNum + 2, randomNum + 3};
+
+
+        SimpleDotCom dot = new SimpleDotCom();
+        dot.setLocationCells(locations);
+
+        boolean isAlive = true;
+        while (isAlive = true) {
+            String guess = helper.getUserInput("insira um numero");
+            String result = dot.checkYourSelf(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+
+                System.out.println("Voce usou " + numOfGuesses + " palpites");
+
+            }
+
+        }
 
     }
+
 }
+
